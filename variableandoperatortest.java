a61b67df5781ea87226da5bfcaee43ce55a9318b
praneethgujarati <praneeth705@gmail.com>
2016-11-13 21:20:40 +0530

    Add files via upload

---
+++ variableandoperatortest.java
@@ -0,0 +1,17 @@
+class variableandoperatortest
+{
+    public static void main(String args[])
+     {
+       int a=20;
+       int b=a-- - --a;
+       int c=a--;
+       int d=a>>2;
+       int e=a&b;
+       System.out.println("The value of a is:"+a);
+       System.out.println("The value of b is:"+b);
+       System.out.println("The value of c is:"+c);
+       System.out.println("The value of d is:"+d);
+       System.out.println("The value of e is:"+e);
+
+      }
+}
